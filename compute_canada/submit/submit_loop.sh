6a4a005b150575ac652f099f6304ca541a29d373
fernando <fjpenaca@uwaterloo.ca>
2023-04-03 16:10:07 -0400

    adding config files
    Former-commit-id: c62d57e8056088e60ba6419f6177e9af24831cc3

--- compute_canada/submit/submit_loop.sh
+++ compute_canada/submit/submit_loop.sh
@@ -2,7 +2,13 @@
 set -e
 array=(
 "configs/edge_consitency/binary_no_water_loss.py"
+"configs/edge_consitency/binary_water_loss_1.py"
+"configs/edge_consitency/binary_water_loss_2_5.py"
+"configs/edge_consitency/binary_water_loss_5.py"
+"configs/edge_consitency/binary_water_loss_25.py"
+"configs/edge_consitency/binary_water_loss_100.py"
 "configs/edge_consitency/no_binary_no_water_loss.py"
+"configs/edge_consitency/no_binary_water_loss_5.py"
 )
 
 wandb_project=edge_consistency
