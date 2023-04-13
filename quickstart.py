130f01659c4b425f95831d2fbe9618a9b109803b
m32patel <m32patel@uwaterloo.ca>
2023-04-13 07:30:26 -0400

    1. add seed 2. get score on valset1 and valset2 3. list of valset added
    Former-commit-id: 00f0babcb159d8ff97b4ba91f5c2144c3de00c6d

--- quickstart.py
+++ quickstart.py
@@ -81,7 +81,8 @@ def parse_args():
     parser.add_argument('config', type=pathlib.Path, help='train config file path',)
     parser.add_argument('--wandb-project', required=True, help='Name of wandb project')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
-    parser.add_argument('--seed', default= None ,help='the seed to use, if not provided, seed from config file will be taken')
+    parser.add_argument('--seed', default=None,
+                        help='the seed to use, if not provided, seed from config file will be taken')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--resume-from', type=pathlib.Path, default=None,
                        help='Resume Training from checkpoint, it will use the \
@@ -302,8 +303,9 @@ def train(cfg, train_options, net, device, dataloader_train, dataloader_val, opt
 
         if train_options['compute_classwise_f1score']:
             from functions import compute_classwise_f1score
+            # dictionary key = chart, value = tensor; e.g  key = SOD, value = tensor([0, 0.2, 0.4, 0.2, 0.1])
             classwise_scores = compute_classwise_f1score(true=inf_ys_flat, pred=outputs_flat,
-                                               charts=train_options['charts'], num_classes=train_options['n_classes'])
+                                                         charts=train_options['charts'], num_classes=train_options['n_classes'])
         print("")
         print(f"Epoch {epoch} score:")
 
@@ -317,7 +319,7 @@ def train(cfg, train_options, net, device, dataloader_train, dataloader_val, opt
             if train_options['compute_classwise_f1score']:
                 for index, class_score in enumerate(classwise_scores[chart]):
                     wandb.log({f"{chart}/Class: {index}": class_score.item()}, step=epoch)
-                print(f"{chart} F1 score:", classwise_scores[chart])    
+                print(f"{chart} F1 score:", classwise_scores[chart])
 
         print(f"Combined score: {combined_score}%")
         print(f"Train Epoch Loss: {train_loss_epoch:.3f}")
@@ -374,10 +376,10 @@ def main():
     # cfg.env_dict = {}
     if not train_options['cross_val_run']:
         # set seed for everything
-        if args.seed != None
-            seed = args.seed
+        if args.seed != None:
+            seed = int(args.seed)
         else:
-            seed = train_options['seed']    
+            seed = train_options['seed']
         random.seed(seed)
         os.environ['PYTHONHASHSEED'] = str(seed)
         np.random.seed(seed)
@@ -513,8 +515,14 @@ def main():
                                 scheduler)
     print('Training Complete')
     print('Testing...')
-    test(False, net, checkpoint_path, device, cfg)
-    test(True, net, checkpoint_path, device, cfg)
+    # todo
+    # this is for valset 1 visualization along with gt
+    test(False, net, checkpoint_path, device, cfg, train_options['val_path'])
+    # todo
+    # this is for valset 2 visualization along with gt
+    test(False, net, checkpoint_path, device, cfg, train_options['test_path'])
+
+    test(True, net, checkpoint_path, device, cfg, train_options['test_challenge_path'])
     print('Testing Complete')
 
     # finish the wandb run
--- test_upload_function.py
+++ test_upload_function.py
@@ -20,13 +20,13 @@ import torch
 import xarray as xr
 from tqdm import tqdm
 # --Proprietary modules -- #
-from functions import chart_cbar, water_edge_plot_overlay
+from functions import chart_cbar, water_edge_plot_overlay, compute_metrics, water_edge_metric, class_decider
 from loaders import AI4ArcticChallengeTestDataset, get_variable_options
 from functions import slide_inference, batched_slide_inference
 import wandb
 
 
-def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
+def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg, cfg_datalist_path):
     """_summary_
 
     Args:
@@ -51,10 +51,16 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
     artifact = wandb.Artifact(experiment_name, 'dataset')
     table = wandb.Table(columns=['ID', 'Image'])
 
+    # - Stores the output and the reference pixels to calculate the scores after inference on all the scenes.
+    outputs_flat = {chart: torch.Tensor().to(device) for chart in train_options['charts']}
+    inf_ys_flat = {chart: torch.Tensor().to(device) for chart in train_options['charts']}
+    # Outputs mask by train fill values
+    outputs_tfv_mask = {chart: torch.Tensor().to(device) for chart in train_options['charts']}
+
     # ### Prepare the scene list, dataset and dataloaders
 
     if test:
-        with open(train_options['path_to_env'] + 'datalists/testset.json') as file:
+        with open(cfg_datalist_path) as file:
             train_options['test_list'] = json.loads(file.read())
             train_options['test_list'] = [file[17:32] + '_' + file[77:80] + '_prep.nc'
                                           for file in train_options['test_list']]
@@ -67,14 +73,14 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
             print('Setup ready')
 
     else:
-        with open(train_options['path_to_env'] + cfg.train_options['val_path']) as file:
+        with open(cfg_datalist_path) as file:
             train_options['test_list'] = json.loads(file.read())
             train_options['test_list'] = [file[17:32] + '_' + file[77:80] + '_prep.nc'
                                           for file in train_options['test_list']]
             # The test data is stored in a separate folder inside the training data.
             upload_package = xr.Dataset()  # To store model outputs.
             dataset = AI4ArcticChallengeTestDataset(
-                options=train_options, files=train_options['test_list'], mode='test_val')
+                options=train_options, files=train_options['test_list'], mode='train_val')
             asid_loader = torch.utils.data.DataLoader(
                 dataset, batch_size=None, num_workers=train_options['num_workers_val'], shuffle=False)
             print('Setup ready')
@@ -87,8 +93,8 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
                                                                total=len(train_options['test_list']), colour='green', position=0):
         scene_name = scene_name[:19]  # Removes the _prep.nc from the name.
         torch.cuda.empty_cache()
-        inf_x = inf_x.to(device, non_blocking=True)
 
+        inf_x = inf_x.to(device, non_blocking=True)
         with torch.no_grad(), torch.cuda.amp.autocast():
             if train_options['model_selection'] == 'swin':
                 output = slide_inference(inf_x, net, train_options, 'test')
@@ -96,10 +102,32 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
             else:
                 output = net(inf_x)
 
-            masks_int = masks.to(torch.uint8)
-            masks_int = torch.nn.functional.interpolate(masks_int.unsqueeze(
-                0).unsqueeze(0), size=original_size, mode='nearest').squeeze().squeeze()
-            masks = torch.gt(masks_int, 0)
+            # output storage as a flat tensor
+            if test is False:
+                for chart in train_options['charts']:
+                    output_tensor = class_decider(output[chart], train_options, chart).detach()
+                    outputs_flat[chart] = torch.cat(
+                        (outputs_flat[chart], output_tensor[~masks[chart]]))
+                    tfv_mask = (inf_x.squeeze()[0, :, :] == train_options['train_fill_value']).squeeze()
+                    outputs_tfv_mask[chart] = torch.cat(
+                        (outputs_tfv_mask[chart], output_tensor[~tfv_mask].to(device)))
+                    inf_ys_flat[chart] = torch.cat(
+                        (inf_ys_flat[chart], inf_y[chart][~masks[chart]].to(device, non_blocking=True)))
+
+            if test:
+                masks_int = masks.to(torch.uint8)
+                masks_int = torch.nn.functional.interpolate(masks_int.unsqueeze(
+                    0).unsqueeze(0), size=original_size, mode='nearest').squeeze().squeeze()
+                masks = torch.gt(masks_int, 0)
+            else:
+                tfv_mask = (inf_x.squeeze()[0, :, :] == train_options['train_fill_value']).squeeze()
+                tfv_mask = torch.nn.functional.interpolate(tfv_mask.type(torch.uint8).unsqueeze(
+                    0).unsqueeze(0), size=original_size, mode='nearest').squeeze().squeeze().to(torch.bool)
+                for chart in train_options['charts']:
+                    masks_int = masks[chart].to(torch.uint8)
+                    masks_int = torch.nn.functional.interpolate(masks_int.unsqueeze(
+                        0).unsqueeze(0), size=original_size, mode='nearest').squeeze().squeeze()
+                    masks[chart] = torch.gt(masks_int, 0)
 
             # masks = torch.nn.functional.interpolate(masks.unsqueeze(0).unsqueeze(0), size = original_size, mode = 'nearest').squeeze().squeeze()
             # Upsample to match the correct size
@@ -108,23 +136,34 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
                     # check if the output is regression output, if yes, permute the dimension
                     if output[chart].size(3) == 1:
                         output[chart] = output[chart].permute(0, 3, 1, 2)
+
+                    # upscale the output
                     output[chart] = torch.nn.functional.interpolate(output[chart], size=original_size, mode='nearest')
+
                     if not test:
                         inf_y[chart] = torch.nn.functional.interpolate(inf_y[chart].unsqueeze(dim=0).unsqueeze(dim=0),
                                                                        size=original_size, mode='nearest')
 
         for chart in train_options['charts']:
             # check if the output is regression output, if yes, round the output to integer
-            if output[chart].size(1) == 1:
-                rounded_tensor = torch.round(output[chart].float()).squeeze().cpu()
-                output[chart] = torch.clamp(rounded_tensor, min=0, max=train_options['n_classes'][chart]).numpy()
-            else:
-                output[chart] = torch.argmax(output[chart], dim=1).squeeze().cpu().numpy()
+            # TODO class decider function in here
+            output[chart] = class_decider(output[chart], train_options, chart)
+            output[chart] = output[chart].cpu().numpy()
             if test:
                 upload_package[f"{scene_name}_{chart}"] = xr.DataArray(name=f"{scene_name}_{chart}", data=output[chart].astype('uint8'),
                                                                        dims=(f"{scene_name}_{chart}_dim0", f"{scene_name}_{chart}_dim1"))
             else:
                 inf_y[chart] = inf_y[chart].squeeze().cpu().numpy()
+
+        # output storage as a flat tensor
+        # if test is False:
+        #     for chart in train_options['charts']:
+        #         outputs_flat[chart] = torch.cat(
+        #             (outputs_flat[chart], torch.tensor(output[chart][~masks[chart]]).to(device)))
+        #         outputs_tfv_mask[chart] = torch.cat(
+        #             (outputs_tfv_mask[chart], torch.tensor(output[chart])[~tfv_mask].to(device)))
+        #         inf_ys_flat[chart] = torch.cat((inf_ys_flat[chart], torch.tensor(inf_y[chart]
+        #                                         [~masks[chart]]).to(device, non_blocking=True)))
 
         # - Show the scene inference.
         if test:
@@ -147,7 +186,7 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
 
         ax = axs[2]
         ax.set_title('Water Edge SIC: Red, SOD: Green,Floe: Blue')
-        edge_water_output = water_edge_plot_overlay(output, masks, train_options)
+        edge_water_output = water_edge_plot_overlay(output, tfv_mask.cpu().numpy(), train_options)
 
         ax.imshow(edge_water_output, vmin=0, vmax=1, interpolation='nearest')
 
@@ -155,7 +194,10 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
 
             ax = axs[idx+3]
             output[chart] = output[chart].astype(float)
-            output[chart][masks] = np.nan
+            if test is False:
+                output[chart][tfv_mask.cpu().numpy()] = np.nan
+            else:
+                output[chart][masks] = np.nan
             ax.imshow(output[chart], vmin=0, vmax=train_options['n_classes']
                       [chart] - 2, cmap='jet', interpolation='nearest')
             ax.set_xticks([])
@@ -169,7 +211,10 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
 
                 ax = axs[idx+6]
                 inf_y[chart] = inf_y[chart].astype(float)
-                inf_y[chart][masks] = np.nan
+                if test is False:
+                    output[chart][tfv_mask.cpu().numpy()] = np.nan
+                else:
+                    output[chart][masks] = np.nan
                 ax.imshow(inf_y[chart], vmin=0, vmax=train_options['n_classes']
                           [chart] - 2, cmap='jet', interpolation='nearest')
                 ax.set_xticks([])
@@ -185,6 +230,33 @@ def test(test: bool, net: torch.nn.modules, checkpoint: str, device: str, cfg):
                     format='png', dpi=128, bbox_inches="tight")
         plt.close('all')
         table.add_data(scene_name, wandb.Image(f"{osp.join(cfg.work_dir,inference_name,scene_name)}.png"))
+
+    if test is False:
+        # compute combine score
+        combined_score, scores = compute_metrics(true=inf_ys_flat, pred=outputs_flat, charts=train_options['charts'],
+                                                 metrics=train_options['chart_metric'], num_classes=train_options['n_classes'])
+
+        # compute water edge metric
+        water_edge_accuarcy = water_edge_metric(outputs_tfv_mask, train_options)
+        if train_options['compute_classwise_f1score']:
+            from functions import compute_classwise_f1score
+            classwise_scores = compute_classwise_f1score(true=inf_ys_flat, pred=outputs_flat,
+                                                         charts=train_options['charts'], num_classes=train_options['n_classes'])
+
+        wandb.run.summary[f"{osp.basename(cfg_datalist_path).split('.')[0]}/Best Combined Score"] = combined_score
+        print(f"{osp.basename(cfg_datalist_path).split('.')[0]}/Best Combined Score = {combined_score}")
+        for chart in train_options['charts']:
+            wandb.run.summary[f"{osp.basename(cfg_datalist_path).split('.')[0]}/{chart} {train_options['chart_metric'][chart]['func'].__name__}"] = scores[chart]
+            print(
+                f"{osp.basename(cfg_datalist_path).split('.')[0]}/{chart} {train_options['chart_metric'][chart]['func'].__name__} = {scores[chart]}")
+            if train_options['compute_classwise_f1score']:
+                wandb.run.summary[f"{osp.basename(cfg_datalist_path).split('.')[0]}/{chart}: classwise score:"] = classwise_scores[chart]
+                print(
+                    f"{osp.basename(cfg_datalist_path).split('.')[0]}/{chart}: classwise score: = {classwise_scores[chart]}")
+
+        wandb.run.summary[f"{osp.basename(cfg_datalist_path).split('.')[0]}/Water Consistency Accuarcy {cfg_datalist_path}"] = water_edge_accuarcy
+        print(
+            f"{osp.basename(cfg_datalist_path).split('.')[0]}/Water Consistency Accuarcy {cfg_datalist_path} = {water_edge_accuarcy}")
 
     if test:
         artifact.add(table, experiment_name+'_test')
