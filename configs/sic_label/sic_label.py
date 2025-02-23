f33fb81e2a298ebedd0ebd99ed791a1ceb8a454a
Junfeng Lei <junf137@outlook.com>
2025-02-23 14:21:05 -0500

    [config] using Pure Python style Configuration File
    see https://mmengine.readthedocs.io/en/latest/advanced_tutorials/config.html for more info

--- configs/sic_label/sic_label.py
+++ configs/sic_label/sic_label.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*-coding:utf-8 -*-
 
+from mmengine.config import read_base
 
-_base_ = ["../_base_/base.py"]
+# Pure Python style Configuration File
+with read_base():
+    from .._base_.base import *
+
+# Pure Text style Configuration File
+# _base_ = ["../_base_/base.py"]
 
 
 train_options = {
