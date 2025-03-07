edd4935ec2a5fc6f790ba7cfc80fb741f2230f74
Junfeng Lei <junf137@outlook.com>
2025-03-07 16:05:33 -0500

    [feat] separate configs for sic_label_mask mask options
    [config] remove mask_ops_maskall.py

---
+++ configs/sic_label/mask_opts_masktrain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+from mmengine.config import read_base
+
+# Pure Python style Configuration File
+with read_base():
+    from .._base_.base import *
+    from .sic_label import *
+
+# Pure Text style Configuration File
+# _base_ = ["../_base_/base.py"]
+
+train_options.update(
+    {
+        "sic_label_mask": {
+            "train": True,
+            "val": False,
+            "test": False,
+        },
+    }
+)
