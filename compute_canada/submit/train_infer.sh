ce8dc98b55ae084475a8438384077cd84b53b8c8
Junfeng Lei <junf137@outlook.com>
2025-03-06 22:13:40 -0500

    [feat] update shell script train_infer.sh and create_env.sh
    1. remove utils/_echo and utils/_error
    2. remove unnecessary comment in train_infer.sh
    3. firstly change dir in train_infer.sh
    4. using 2 parameters in create_env.sh for ENV_BASE_DIR and ENV_NAME
    5. remove adding path for utils in train_infer.sh and create_env.sh

--- compute_canada/submit/train_infer.sh
+++ compute_canada/submit/train_infer.sh
@@ -13,47 +13,27 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
 
-export PATH="./utils:$PATH"
+# Change to the repo directory
+cd $HOME/projects/rrg-dclausi/$USER/ai4arctic_challenge_clean
 
 # Usage: sbatch train_infer.sh <config> <wandb_project> <seed> <venv_path>
 if [ "$#" -ne 4 ]; then
-    _error "Usage: sbatch ${0##*/} <config> <wandb_project> <seed> <venv_path>"
+    echo "---* Usage: sbatch ${0##*/} <config> <wandb_project> <seed> <venv_path>"
     exit 1
 fi
 
 # Purge all loaded modules
-_echo "Purging all loaded modules..."
+echo "---* Purging all loaded modules..."
 module --force purge
 
 # Load necessary modules
-_echo "Loading required modules..."
+echo "---* Loading required modules..."
 module load StdEnv gcc opencv/4.10.0
 module load python/3.10.13
-
 
 # Activate the virtual environment
 source $4/bin/activate
 
-_echo "Running the python script..."
-
-# Change to the repo directory
-cd $HOME/projects/rrg-dclausi/$USER/ai4arctic_challenge_clean
-
-# config=$1
-# # get the basename for the config file, basename is an inbuilt shell command
-# config_basename=$(basename $config .py)
-
-
+echo "---* Running the python script..."
 export WANDB_MODE=offline
 python quickstart.py $1 --wandb-project=$2 --seed=$3
-
-
-# # the above python script will generate a .env at the workdir/config-name/.env
-# env=./work_dir/$config_basename/.env
-
-# echo 'Reading environment file'
-# # read the .env file and save them as environment variable
-# while read line; do export $line; done < $env
-
-# echo "Starting testing"
-# python test_upload.py $1 $CHECKPOINT
--- create_env.sh
+++ create_env.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 # Usage: bash create_env_new.sh <env_name>
-
-# Ensure an environment name is provided
-if [ -z "$1" ]; then
-    echo "Error: No environment name provided."
-    echo "Usage: bash create_env_new.sh <env_name>"
+if [ "$#" -ne 2 ]; then
+    echo "---* Usage: bash ${0##*/} <env_folder> <env_name>"
     exit 1
 fi
 
-export PATH="./utils:$PATH"
-
-ENV_NAME=$1
-ENV_BASE_DIR=~/.venvs
+ENV_NAME=$2
+ENV_BASE_DIR=$1
 ENV_DIR=$ENV_BASE_DIR/$ENV_NAME
 
 # Create the base directory if it doesn't exist
@@ -30,16 +25,16 @@ if [ -d "$ENV_DIR" ]; then
 fi
 
 # Purge all loaded modules
-_echo "Purging all loaded modules..."
+echo "---* Purging all loaded modules..."
 module --force purge
 
 # Load necessary modules
-_echo "Loading required modules..."
-module load StdEnv gcc opencv
+echo "---* Loading required modules..."
+module load StdEnv gcc opencv/4.10.0
 module load python/3.10.13
 
 # Create virtual environment
-_echo "Creating virtual environment: $ENV_NAME in $ENV_DIR"
+echo "---* Creating virtual environment: $ENV_NAME in $ENV_DIR"
 virtualenv "$ENV_DIR"
 if [ $? -ne 0 ]; then
     echo "Error: Failed to create virtual environment."
@@ -50,19 +45,19 @@ fi
 source "$ENV_DIR/bin/activate"
 
 # Upgrade pip and install dependencies
-_echo "Upgrading pip and installing dependencies..."
+echo "---* Upgrading pip and installing dependencies..."
 pip install --no-index --upgrade pip
 pip install --ignore-installed numpy wandb==0.16.0 Pillow pandas matplotlib
 pip install mmcv h5netcdf tqdm scikit-learn jupyterlab ipywidgets icecream xarray seaborn cmocean \
             torch torchvision torchmetrics torch-summary segmentation_models_pytorch
 
 # Check whether all the packages are installed
-_echo "Checking installed packages..."
+echo "---* Checking installed packages..."
 packages=("numpy" "wandb" "mmcv" "h5netcdf" "Pillow" "pandas" "tqdm" "scikit-learn" "jupyterlab" "ipywidgets" "icecream" "matplotlib" "xarray" "seaborn" "cmocean" "torch" "torchvision" "torchmetrics" "torch-summary" "segmentation_models_pytorch")
 for package in "${packages[@]}"; do
     if ! python -m pip show -q "$package"; then
-        _error "Error: Package '$package' not properly installed"
+        echo "---* Error: Package '$package' not properly installed"
     fi
 done
 
-_echo "Environment setup complete! Activate using: source $ENV_DIR/bin/activate"
+echo "---* Environment setup complete! Activate using: source $ENV_DIR/bin/activate"
