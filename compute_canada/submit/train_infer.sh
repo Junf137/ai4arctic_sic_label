e4bb7ef081058b1eef5536cb194b09c83fc66c99
Junfeng Lei <3086638517@qq.com>
2025-03-09 19:38:39 -0400

    [feat] add train_infer.sh and configs/sic_label

--- compute_canada/submit/train_infer.sh
+++ compute_canada/submit/train_infer.sh
@@ -1,46 +1,39 @@
 #!/bin/bash
-#SBATCH --nodes 1
-#SBATCH --gpus-per-node=1 # request a GPU
-#SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=12 # change this parameter to 2,4,6,... and increase "--num_workers" accordingly to see the effect on performance
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=12
 #SBATCH --mem=128G
-#SBATCH --time=2:00:00
-#SBATCH --output=/home/fer96/projects/def-dclausi/AI4arctic/fer96/ai4arctic_challenge_clean/compute_canada_output/%j.out
-#SBATCH --account=def-ka3scott
-#SBATCH --mail-user=FernandoComputeCanada@gmail.com
+#SBATCH --time=6:59:00
+#SBATCH --output=/home/j46lei/projects/rrg-dclausi/j46lei/ai4arctic_challenge_clean/output/%u_%x_%j.log
+#SBATCH --account=rrg-dclausi
+#SBATCH --mail-user=junf137@outlook.com
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
 
-module purge
-module load python/3.10
+# Change to the repo directory
+cd $HOME/projects/rrg-dclausi/$USER/ai4arctic_challenge_clean
 
-echo "Loading module done"
+# Usage: sbatch train_infer.sh <config> <wandb_project> <seed> <venv_path>
+if [ "$#" -ne 4 ]; then
+    echo "---* Usage: sbatch ${0##*/} <config> <wandb_project> <seed> <venv_path>"
+    exit 1
+fi
 
-source ~/AI4Artic2/bin/activate
+# Purge all loaded modules
+echo "---* Purging all loaded modules..."
+module --force purge
 
-echo "Activating virtual environment done"
+# Load necessary modules
+echo "---* Loading required modules..."
+module load StdEnv gcc opencv/4.10.0
+module load python/3.10.13
 
-cd $HOME/projects/def-dclausi/AI4arctic/$USER/ai4arctic_challenge_clean/
+# Activate the virtual environment
+source $4/bin/activate
 
-
-echo "starting training..."
-# config=$1 
-# # get the basename for the config file, basename is an inbuilt shell command
-# config_basename=$(basename $config .py) 
-
-
+echo "---* Running the python script..."
 export WANDB_MODE=offline
 python quickstart.py $1 --wandb-project=$2 --seed=$3
-
-
-# # the above python script will generate a .env at the workdir/config-name/.env
-# env=./work_dir/$config_basename/.env
-
-# echo 'Reading environment file'
-# # read the .env file and save them as environment variable
-# while read line; do export $line; done < $env
-
-# echo "Starting testing"
-# python test_upload.py $1 $CHECKPOINT
---
+++ configs/sic_label/sic_label.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+from mmengine.config import read_base
+
+# Pure Python style Configuration File
+with read_base():
+    from .._base_.base import *
+
+# Pure Text style Configuration File
+# _base_ = ["../_base_/base.py"]
+
+
+train_options.update(
+    {
+        # -- General configuration -- #
+        "path_to_train_data": "/home/j46lei/projects/rrg-dclausi/ai4arctic/dataset/dataset_ready_to_train/train",
+        "path_to_test_data": "/home/j46lei/projects/rrg-dclausi/ai4arctic/dataset/dataset_ready_to_train/test",
+        "train_list_path": "datalists/dataset.json",  # Training dataset list.
+        "val_path": "datalists/valset2.json",  # Validation dataset list. Disabled when cross_val_run is True.
+        "test_path": "datalists/testset.json",  # Test dataset list.
+        # -- Experiment configuration -- #
+        "cross_val_run": True,
+        "p-out": 20,  # number of scenes taken from the TRAIN SET as the validation set.
+        "compute_classwise_f1score": True,
+        "plot_confusion_matrix": True,
+        "num_workers": 4,  # Number of parallel processes to fetch data.
+        "num_workers_val": 4,  # Number of parallel processes during validation.
+        "down_sample_scale": 10,
+        # -- SIC masking configuration -- #
+        "sic_label_mask": {
+            "train": True,
+            "val": True,
+            "test": False,
+            "ksize_ratio": 100,  # ratio of the (image size / kernel size)
+            "mask_threshold": 0,  # threshold for determining the mask after sobel filter
+            "visualization": False,
+            # Path to save visualization. Only works when visualization is True.
+            "visualization_save_path": "/home/j46lei/projects/rrg-dclausi/j46lei/ai4arctic_challenge_clean/output/visualization",
+        },
+        # -- Training configuration -- #
+        "epochs": 300,
+        "epoch_len": 500,  # Number of batches for each epoch.
+        "patch_size": 256,
+        "batch_size": 16,
+        "model_selection": "unet_regression",  #'unet_feature_fusion', #'unet_regression',
+        "unet_conv_filters": [32, 32, 64, 64],
+        "optimizer": {
+            "type": "SGD",
+            "lr": 0.001,  # Optimizer learning rate.
+            "momentum": 0.9,
+            "dampening": 0,
+            "nesterov": False,
+            "weight_decay": 0.01,
+        },
+        "scheduler": {
+            "type": "CosineAnnealingWarmRestartsLR",
+            "EpochsPerRestart": 20,  # Number of epochs for the first restart
+            # This number will be used to increase or decrease the number of epochs to restart after each restart.
+            "RestartMult": 1,
+            "lr_min": 0,
+        },
+        "data_augmentations": {
+            "Random_h_flip": 0.5,
+            "Random_v_flip": 0.5,
+            "Random_rotation_prob": 0.5,
+            "Random_rotation": 90,
+            "Random_scale_prob": 0.5,
+            "Random_scale": (0.9, 1.1),
+            "Cutmix_beta": 1.0,
+            "Cutmix_prob": 0.5,
+        },
+        # -- Loss configuration -- #
+        "task_weights": [1, 3, 3],
+        "chart_loss": {
+            "SIC": {
+                "type": "MSELossWithIgnoreIndex",
+                "ignore_index": 255,
+            },
+            "SOD": {
+                "type": "CrossEntropyLoss",
+                "ignore_index": 255,
+            },
+            "FLOE": {
+                "type": "CrossEntropyLoss",
+                "ignore_index": 255,
+            },
+        },
+    }
+)
