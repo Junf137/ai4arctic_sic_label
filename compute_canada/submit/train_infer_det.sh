7ac4e9495f7dc57a31b9af6a088abda61ca6f219
Junfeng Lei <3086638517@qq.com>
2025-03-12 20:20:48 -0400

    [config] add config and sh for deterministic feature

---
+++ compute_canada/submit/train_infer_det.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=128G
+#SBATCH --time=6:59:00
+#SBATCH --output=/home/j46lei/projects/rrg-dclausi/j46lei/ai4arctic_challenge_clean/output/%u_%x_%j.log
+#SBATCH --account=rrg-dclausi
+#SBATCH --mail-user=junf137@outlook.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+
+# Usage: sbatch train_infer.sh <config> <wandb_project> <seed> <venv_path>
+if [ "$#" -ne 4 ]; then
+    echo "---* Usage: sbatch ${0##*/} <config> <wandb_project> <seed> <venv_path>"
+    exit 1
+fi
+
+# Purge all loaded modules
+echo "---* Purging all loaded modules..."
+module --force purge
+
+# Load necessary modules
+echo "---* Loading required modules..."
+module load StdEnv gcc opencv/4.10.0
+module load python/3.10.13
+
+# Activate the virtual environment
+source $4/bin/activate
+
+echo "---* Running the python script..."
+export WANDB_MODE=offline
+# for cublas reproducibility
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+python quickstart.py $1 --wandb-project=$2 --seed=$3
