9f05a55fb638cb1cbcfb3685b0af7e8d594672f1
Junfeng Lei <junf137@outlook.com>
2025-04-20 15:40:16 -0400

    [fix] update batch_submit with changes in train_infer.sh

--- compute_canada/submit/batch_submit.sh
+++ compute_canada/submit/batch_submit.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Usage: bash batch_submit.sh <submit_num> <wandb_project> <seed> <venv_path>
+# Usage: bash batch_submit.sh <job_script> <submit_num> <venv_path> <wandb_project> <seed> <other_args...>
 if [ "$#" -ne 5 ]; then
-    echo "---* Usage: bash ${0##*/} <job_script> <submit_num> <wandb_project> <seed> <venv_path>"
+    echo "---* Usage: bash ${0##*/} <job_script> <submit_num> <venv_path> <wandb_project> <seed> <other_args...>"
     exit 1
 fi
 
 # Set the variables
 job_script=$1
-submit_num=$2
-wandb_project=$3
-seed=$4
-venv_path=$5
+shift
+submit_num=$1
+shift
+venv_path=$1
+shift
+wandb_project=$1
+shift
+seed=$1
+shift
+other_args="$@"
 
 # Set the config array
 config_array=(
@@ -21,10 +27,10 @@ config_array=(
 
 # loop submission
 for i in "${!config_array[@]}"; do
-    echo "sbatch $job_script ${config_array[i]} $wandb_project $seed $venv_path"
+    echo "sbatch $job_script $venv_path ${config_array[i]} $wandb_project $seed $other_args"
 
     for j in $(seq 1 $submit_num); do
-        sbatch $job_script ${config_array[i]} $wandb_project $seed $venv_path
+        sbatch $job_script $venv_path ${config_array[i]} $wandb_project $seed $other_args
         sleep 3
     done
 done
